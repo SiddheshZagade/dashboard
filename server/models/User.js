a4528d7b89e684aee5b1af454c57d36e735176af
SiddheshZagade <siddeshzagade7@gmail.com>
2023-03-08 00:23:01 +0530

    tut1

--- server/models/User.js
+++ server/models/User.js
@@ -1,78 +1,38 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  end_year: {
-    type: Number,
-    default: null
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    city: String,
+    state: String,
+    country: String,
+    occupation: String,
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+      type: String,
+      enum: ["user", "admin", "superadmin"],
+      default: "admin",
+    },
   },
-  intensity: {
-    type: Number,
-    
-  },
-  sector: {
-    type: String,
-    
-  },
-  topic: {
-    type: String,
-    
-  },
-  insight: {
-    type: String,
-    
-  },
-  url: {
-    type: String,
-    
-  },
-  region: {
-    type: String,
-    default: null
-  },
-  start_year: {
-    type: Number,
-    default: null
-  },
-  impact: {
-    type: String,
-    default: null
-  },
-  added: {
-    type: Date,
-   
-    default: Date.now
-  },
-  published: {
-    type: Date,
-    
-    default: Date.now
-  },
-  country: {
-    type: String,
-    default: null
-  },
-  relevance: {
-    type: Number,
-   
-    default: 0
-  },
-  pestle: {
-    type: String,
-    
-  },
-  source: {
-    type: String,
-    
-  },
-  title: {
-    type: String,
-    
-  },
-  likelihood: {
-    type: Number,
-    
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 export default User;
