d178e629e99e89f3e45f92bd27d719074822a364
SiddheshZagade <siddeshzagade7@gmail.com>
2023-03-08 18:12:42 +0530

    fix env

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,26 +1,13 @@
 // client/src/scenes/dashboard/index.jsx
-
-import React, { useState } from "react";
-import { useGetUserQuery } from "../../state/api";
-
+import React from 'react'
 
 const Dashboard = () => {
-  const [userId, setUserId] = useState("613bfcaeb7b07c450a28d45b");
-  const { data, isLoading, isError } = useGetUserQuery(userId);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error fetching user data</div>;
-  }
-
   return (
     <div>
-      user={data?.data} 
+      Dashboard
+
     </div>
-  );
-};
+  )
+}
 
 export default Dashboard;
