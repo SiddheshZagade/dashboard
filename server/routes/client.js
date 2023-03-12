276747864f6c6f585abfd0b7fd040cc82df9c8fd
SiddheshZagade <siddeshzagade7@gmail.com>
2023-03-12 20:26:10 +0530

    chart1

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-  getProducts,getCustomers,getTransactions
+  getProducts,getCustomers,getTransactions,getGeography
   
 } from "../controllers/client.js";
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions",getTransactions);
+router.get("/geography",getGeography);
 
 export default router;
